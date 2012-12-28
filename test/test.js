e8b157029fc68988769f71e01033a0f0cc5971be
Chris Cykana <chris@cykana.com>
2012-12-28 13:41:40 -0600

    refactored getUnitDescription, removed getAvailable units

--- test/test.js
+++ test/test.js
@@ -10,18 +10,18 @@ describe('units', function () {
       units.getUnitDescription(uuid).should.eql('Teaspoon');
       done();
     });
-  });
-  
-  describe.skip('getAvailableUnits()', function () {
     it('should translate a group of uuids into a group of english measurement units', function (done) {
       var uuids = [
         'urn:uuid:c2a18c35-f4e7-4297-b8de-1e0f1bd16cfb'
       , 'urn:uuid:1c8d43b3-6b19-43ab-95f0-803a37fdf4f1'
       , 'urn:uuid:dfad1d25-17ff-4201-bba0-0711e8b88c65'
       ];
-      var actual = units.getAvailableUnits(uuids);
       units.getAvailableUnits(uuids).should.eql(['Teaspoon', 'Tablespoon', 'Cup']);
       done();
-    });    
+    }); 
+  });
+  
+  describe.skip('getAvailableUnits()', function () {
+       
   });
 });
