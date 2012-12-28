5a48954c2b12aa011ab69180db283c08212cc55d
Chris Cykana <chris@cykana.com>
2012-12-28 14:15:24 -0600

    searchFoods()

--- esha.js
+++ esha.js
@@ -2,43 +2,41 @@ var http = require('http')
   , qs = require('querystring')
   , _ = require('underscore')
   , units = require('./lib/units')
-  , api_key = '3gydgk8hnvvxye9ppj7vn8f9'
-  , food = process.argv[2]
-  , query = qs.stringify({
-      query: food
-    , apikey: api_key
-    })
-  , options = {
-      host: 'api.esha.com'
-    , port: 80
-    , path: '/foods?' + query
-    };
 
-http.get(options, function (res) {
-  var data = '';
-  res.on('data', function (chunk) {
-    data += chunk.toString();
-  })
-  .on('error', function (err) {
-    console.err('Error retrieveing ESHA food units');
-    throw err;
-  })
-  .on('end', function () {
-    var results = JSON.parse(data);
-    var results_display = [];
-    _.each(results.items, function (r) {
-      var result = {
-        item: r.description
-      , default_unit: units.getUnitDescription(r.unit)
-      , available_units: units.getAvailableUnits(r.units)
-      , product: r.product || false
-      , supplier: r.supplier || false
-      }
-      results_display.push(result);
+var api_key = '3gydgk8hnvvxye9ppj7vn8f9'
+var options = { host: 'api.esha.com', port: 80 };
+
+function searchFoods (food) {
+  var query = qs.stringify({ query: food, apikey: api_key });
+  options.path = '/foods?' + query;
+  http.get(options, function (res) {
+    var data = '';
+    res.on('data', function (chunk) {
+      data += chunk.toString();
+    })
+    .on('error', function (err) {
+      console.err('Error retrieveing ESHA food units');
+      throw err;
+    })
+    .on('end', function () {
+      var results = JSON.parse(data);
+      var results_display = [];
+      _.each(results.items, function (r) {
+        var result = {
+          item: r.description
+        , group: r.group
+        , product: r.product || ''
+        , supplier: r.supplier || ''
+        , default_unit: units.getUnitDescription(r.unit)
+        , available_units: units.getUnitDescription(r.units)        
+        }
+        results_display.push(result);
+      });
+      console.log(results_display);
+      console.log("Query: " + results.query + ", displaying " + 
+        results.items.length + " of " + results.total + " results.");
     });
-    console.log(results_display);
-    console.log("Query: " + results.query + 
-                ", Total Results: " + results.total
-               );
   });
-});
+}
+
+searchFoods(process.argv[2]);
--- test/test.js
+++ test/test.js
@@ -5,23 +5,24 @@ var assert = require('assert')
 
 describe('units', function () {
   describe('getUnitDescription()', function () {
-    it('should translate a uuid into an english measurement unit', function (done) {
+    it('should translate a uuid into a human-readable unit', function (done) {
       var uuid = 'urn:uuid:c2a18c35-f4e7-4297-b8de-1e0f1bd16cfb';
       units.getUnitDescription(uuid).should.eql('Teaspoon');
       done();
     });
-    it('should translate a group of uuids into a group of english measurement units', function (done) {
+    it('should translate a group of uuids into a group of human-readable units', function (done) {
       var uuids = [
         'urn:uuid:c2a18c35-f4e7-4297-b8de-1e0f1bd16cfb'
       , 'urn:uuid:1c8d43b3-6b19-43ab-95f0-803a37fdf4f1'
       , 'urn:uuid:dfad1d25-17ff-4201-bba0-0711e8b88c65'
       ];
-      units.getAvailableUnits(uuids).should.eql(['Teaspoon', 'Tablespoon', 'Cup']);
+      units.getUnitDescription(uuids).should.eql(['Teaspoon', 'Tablespoon', 'Cup']);
       done();
     }); 
   });
-  
-  describe.skip('getAvailableUnits()', function () {
-       
+});
+
+describe('esha', function () {
+  describe.skip('searchFoods()', function () {
   });
 });
